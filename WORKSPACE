ebf5868597d4a19deb97122269292fffbf7e03a0
Jacob Deery <jacob.deery@gmail.com>
2020-01-11 22:56:57 -0500

    Add buildifier to format script

--- WORKSPACE
+++ WORKSPACE
@@ -8,4 +8,5 @@ git_repository(
 )
 
 load("@com_github_nelhage_rules_boost//:boost/boost.bzl", "boost_deps")
+
 boost_deps()
--- format.sh
+++ format.sh
@@ -4,4 +4,10 @@ WORKSPACE_DIR=$(bazel info workspace)
 
 cd $WORKSPACE_DIR
 
+echo "Running clang-format..."
 git status -s -u | grep '\.cpp$\|\.h$' | sed "s/^.\{3\}//" | xargs clang-format -i
+
+echo "Running buildifier..."
+git status -s -u | grep 'WORKSPACE\|BUILD\|\.bzl$' | sed "s/^.\{3\}//" | xargs buildifier
+
+echo "Formatting completed."
