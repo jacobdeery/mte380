862b35c85b2653767ea47b9671583fdac49f8f2d
Jacob Deery <jacob.deery@gmail.com>
2020-02-06 00:05:41 -0500

    Make serialization tests more representative

--- source/core/bus/tests/serialization_tests.cpp
+++ source/core/bus/tests/serialization_tests.cpp
@@ -9,9 +9,14 @@ class SerializableClass {
     SerializableClass() = default;
     SerializableClass(int x, const std::string& s) : x{x}, s{s} {}
 
+    int X() const { return x; };
+    std::string S() const { return s; };
+
+   private:
     int x;
     std::string s;
 
+   public:
     MSGPACK_DEFINE(x, s);
 };
 
@@ -28,6 +33,6 @@ TEST(SerializationTests, CustomClassRoundTrip) {
     const auto buf = Serialize(stc);
     const auto deserialized = Deserialize<SerializableClass>(buf);
 
-    EXPECT_EQ(stc.x, deserialized.x);
-    EXPECT_EQ(stc.s, deserialized.s);
+    EXPECT_EQ(stc.X(), deserialized.X());
+    EXPECT_EQ(stc.S(), deserialized.S());
 }
