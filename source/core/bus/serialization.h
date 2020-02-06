62734aede3d8882e3fa7e124034940097a2bb8f1
Jacob Deery <jacob.deery@gmail.com>
2020-02-05 22:23:53 -0500

    Add serialization via msgpack
    Data types can now be serialized! Use the `Serialize` and
    `Deserialize<T>` functions to convert to and from msgpack buffers.

---
+++ source/core/bus/serialization.h
@@ -0,0 +1,16 @@
+#include <msgpack.hpp>
+
+template <class T>
+msgpack::sbuffer Serialize(const T& gen_obj) {
+    msgpack::sbuffer buf;
+    msgpack::pack(buf, gen_obj);
+    return buf;
+}
+
+template <class T>
+T Deserialize(const msgpack::sbuffer& buf) {
+    T gen_obj;
+    msgpack::object_handle oh = msgpack::unpack(buf.data(), buf.size());
+    msgpack::object obj = oh.get();
+    return obj.convert(gen_obj);
+}
---
+++ source/core/bus/tests/serialization_tests.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+
+#include "source/core/bus/serialization.h"
+
+class SerializableClass {
+   public:
+    SerializableClass() = default;
+    SerializableClass(int x, const std::string& s) : x{x}, s{s} {}
+
+    int x;
+    std::string s;
+
+    MSGPACK_DEFINE(x, s);
+};
+
+TEST(SerializationTests, IntRoundTrip) {
+    const int i = 5;
+    const auto buf = Serialize(i);
+    const auto deserialized = Deserialize<int>(buf);
+
+    EXPECT_EQ(i, deserialized);
+}
+
+TEST(SerializationTests, CustomClassRoundTrip) {
+    const SerializableClass stc(10, "mte");
+    const auto buf = Serialize(stc);
+    const auto deserialized = Deserialize<SerializableClass>(buf);
+
+    EXPECT_EQ(stc.x, deserialized.x);
+    EXPECT_EQ(stc.s, deserialized.s);
+}
