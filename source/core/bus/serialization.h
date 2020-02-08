cd347d10eebfcb8eaabd39e6987db2c1e64dc86e
Jacob Deery <jacob.deery@gmail.com>
2020-02-07 20:57:40 -0500

    Use stringstreams as buffers

--- source/core/bus/serialization.h
+++ source/core/bus/serialization.h
@@ -6,14 +6,14 @@ namespace mte {
 namespace bus {
 
 template <class T>
-msgpack::sbuffer Serialize(const T& gen_obj) {
-    msgpack::sbuffer buf;
-    msgpack::pack(buf, gen_obj);
-    return buf;
+std::string Serialize(const T& gen_obj) {
+    std::stringstream ss;
+    msgpack::pack(ss, gen_obj);
+    return ss.str();
 }
 
 template <class T>
-T Deserialize(const msgpack::sbuffer& buf) {
+T Deserialize(const std::string& buf) {
     T gen_obj;
     msgpack::object_handle oh = msgpack::unpack(buf.data(), buf.size());
     msgpack::object obj = oh.get();
