545c95797cf470dfdc7912e29c4dfe6c660818fb
Jacob Deery <jacob.deery@gmail.com>
2020-02-22 18:03:56 -0500

    Update the build script for different configs
    New usage: ./build.sh [-c config] [-d]. Without -c, builds the default
    config. With -d, runs deploy.sh after the build is completed.

--- build.sh
+++ build.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-bazel build //source/...
+usage()
+{
+  echo "Usage: $0 [-c CONFIG_NAME] [-d]"
+  exit 2
+}
+
+unset CONFIG DEPLOY
+
+while getopts 'c:dh?' c
+do
+  case $c in
+    c) CONFIG=$OPTARG ;;
+    d) DEPLOY=1 ;;
+    h|?) usage ;;
+  esac
+done
+
+if [ -n "$CONFIG" ]; then
+  bazel build //source/... --config=$CONFIG
+else
+  bazel build //source/...
+fi
+
+if [ -n "$DEPLOY" ]; then
+  ./deploy.sh
+fi
