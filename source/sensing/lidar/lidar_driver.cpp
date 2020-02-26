f9bc91e4688d4ec1b91844e51d519a76db848dc9
Jacob Deery <jacob.deery@gmail.com>
2020-02-25 23:58:20 -0500

    Fix the loop rate (2 Hz)

--- source/sensing/lidar/lidar_driver.cpp
+++ source/sensing/lidar/lidar_driver.cpp
@@ -5,8 +5,11 @@
 #include <chrono>
 #include <thread>
 
-using namespace mte;
 using namespace std::chrono_literals;
+
+constexpr auto kLoopDuration{500ms};
+
+using namespace mte;
 
 int main() {
     InitializeLogging("lidar_driver");
@@ -19,11 +22,15 @@ int main() {
     }
 
     while (true) {
+        const auto loop_start = std::chrono::system_clock::now();
+
         const auto scan = lidar_bridge.Scan();
         if (scan.has_value()) {
             LOG_INFO(scan.value().data.size());
         } else {
             LOG_WARN("Lidar scan had no value");
         }
+
+        std::this_thread::sleep_until(loop_start + kLoopDuration);
     }
 }
